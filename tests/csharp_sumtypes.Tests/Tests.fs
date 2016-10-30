ab3a4c2263e0a5f1b97b4f5fe5f70df4e353f082
John Azariah <john.azariah@gmail.com>
2016-10-29 20:41:23 -0700

    ProjectScaffold Initialized

---
+++ tests/csharp_sumtypes.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module csharp_sumtypes.Tests
+
+open csharp_sumtypes
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
