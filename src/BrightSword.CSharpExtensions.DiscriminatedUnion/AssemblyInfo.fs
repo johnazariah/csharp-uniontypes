e13a1cd283b0a15a323cac845d8c60de9e842fd1
John Azariah <john.azariah@gmail.com>
2016-12-31 15:29:33 -0800

    Bump version to 1.0.0

--- src/BrightSword.CSharpExtensions.DiscriminatedUnion/AssemblyInfo.fs
+++ src/BrightSword.CSharpExtensions.DiscriminatedUnion/AssemblyInfo.fs
@@ -6,15 +6,15 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("BrightSword.CSharpExtensions.DiscriminatedUnion")>]
 [<assembly: AssemblyProductAttribute("BrightSword.CSharpExtensions.DiscriminatedUnion")>]
 [<assembly: AssemblyDescriptionAttribute("DSL, Parser & Code Generator for Discriminated Union Types (Sum Types) in C#")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
 [<assembly: InternalsVisibleToAttribute("BrightSword.CSharpExtensions.DiscriminatedUnion.Tests")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "BrightSword.CSharpExtensions.DiscriminatedUnion"
     let [<Literal>] AssemblyProduct = "BrightSword.CSharpExtensions.DiscriminatedUnion"
     let [<Literal>] AssemblyDescription = "DSL, Parser & Code Generator for Discriminated Union Types (Sum Types) in C#"
-    let [<Literal>] AssemblyVersion = "0.0.2"
+    let [<Literal>] AssemblyVersion = "1.0.0"
     let [<Literal>] InternalsVisibleTo = "BrightSword.CSharpExtensions.DiscriminatedUnion.Tests"
-    let [<Literal>] AssemblyFileVersion = "0.0.2"
+    let [<Literal>] AssemblyFileVersion = "1.0.0"
